4067b71f6b5fae1fc5c75492f4fc7dc02eed5086
oidohou <nemetoninc@gmail.com>
2023-05-17 17:07:16 -0400

    feat(links-components): Add new core components for Links

---
+++ src/core/core-button-link/__tests__/ButtonLink.spec.js
@@ -0,0 +1,114 @@
+import React from "react";
+import { mount, shallow, render } from "enzyme";
+
+import ButtonLink from "../ButtonLink";
+import { warn } from "../../../utils/warn";
+import A11yContent from "../../core-a11y-content/A11yContent";
+
+jest.mock("../../../utils/warn");
+
+describe("ButtonLink", () => {
+  const doMount = (overrides = {}) => {
+    const link = mount(<ButtonLink {...overrides}>Go home</ButtonLink>);
+
+    return link.find("a");
+  };
+
+  const doShallow = (overrides = {}) => {
+    const link = shallow(<ButtonLink {...overrides}>Go home</ButtonLink>);
+
+    return link;
+  };
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("renders", () => {
+    const link = render(<ButtonLink>Go home</ButtonLink>);
+
+    expect(link).toMatchSnapshot();
+  });
+
+  it("is an anchor HTML element when using the href attribute", () => {
+    const link = doMount({ href: "http://nds.com" });
+
+    expect(link).toHaveDisplayName("a");
+    expect(link).toHaveProp("href", "http://nds.com");
+  });
+
+  it("renders a react router link element when passed as a prop", () => {
+    const MyLink = () => <span />;
+    const link = mount(
+      <ButtonLink reactRouterLinkComponent={MyLink}>The link test</ButtonLink>
+    );
+
+    expect(link.find(MyLink)).toExist();
+  });
+
+  it("must use `reactRouterLinkComponent` and `to` props together", () => {
+    const MyLink = () => <span />;
+    doShallow({ reactRouterLinkComponent: MyLink });
+
+    expect(warn).toHaveBeenCalled();
+
+    jest.clearAllMocks();
+
+    const link = doShallow({ to: "/about" });
+
+    expect(link).toHaveProp("to");
+    expect(warn).toHaveBeenCalled();
+  });
+
+  it("can be presented as one of the allowed variants", () => {
+    const link = render(<ButtonLink variant="secondary">Go home</ButtonLink>);
+
+    expect(link).toMatchSnapshot();
+  });
+
+  it("can be presented as one of the allowed variants", () => {
+    const link = render(
+      <ButtonLink variant="standard" rank="main">
+        Go home
+      </ButtonLink>
+    );
+
+    expect(link).toMatchSnapshot();
+  });
+
+  it("can be presented as one of the allowed fullwidth", () => {
+    const link = render(<ButtonLink fullWidth>Go home</ButtonLink>);
+
+    expect(link).toMatchSnapshot();
+  });
+
+  it("passes additional attributes to link element", () => {
+    const link = doShallow({ id: "the-link", tabIndex: 1 });
+
+    expect(link).toHaveProp("id", "the-link");
+    expect(link).toHaveProp("tabIndex", 1);
+  });
+
+  describe("A11yContent", () => {
+    it("connects to ButtonLink", () => {
+      const link = shallow(
+        <ButtonLink>
+          Go home
+          <A11yContent>testing</A11yContent>
+        </ButtonLink>
+      );
+
+      expect(link).toContainReact(<A11yContent>testing</A11yContent>);
+    });
+  });
+
+  it("does not allow custom CSS", () => {
+    const link = doShallow({
+      className: "my-custom-class",
+      style: { color: "hotpink" },
+    });
+
+    expect(link).not.toHaveProp("className", "my-custom-class");
+    expect(link).not.toHaveProp("style");
+  });
+});
--- src/core/core-link/Link.js
+++ src/core/core-link/Link.js
@@ -1,118 +1,142 @@
-import React, { useCallback } from 'react'
-import PropTypes from 'prop-types'
-import styled from 'styled-components'
-import { colorGreyShark, colorWhite, colorGainsboro, colorGreyRaven } from '../core-colours/colours'
-import { links } from '../../shared-styles'
-import { safeRest } from '../../util-helpers'
-import { componentWithName } from '../../util-prop-types'
-import { withForwardedRef } from '../../shared-hocs'
-import { warn } from '../../utils/warn'
+import React, { useCallback } from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import {
+  colorGreyShark,
+  colorWhite,
+  colorGainsboro,
+  colorGreyRaven,
+} from "../core-colours/colours";
+import { links } from "../../shared-styles";
+import { safeRest } from "../../util-helpers";
+import { componentWithName } from "../../util-prop-types";
+import { withForwardedRef } from "../../shared-hocs";
+import { warn } from "../../utils/warn";
 
 const base = {
   ...links.focusOutline,
-  '&:link,&:visited': {
+  "&:link,&:visited": {
     color: colorGreyShark,
-    textDecoration: 'underline',
+    textDecoration: "underline",
   },
-  '&:hover': {
-    textDecoration: 'none',
+  "&:hover": {
+    textDecoration: "none",
   },
-  '& svg': {},
-}
+  "& svg": {},
+};
 
 const states = ({ invert }) => {
   return {
-    '&:active': {
+    "&:active": {
       color: invert && colorGainsboro,
-      backgroundColor: invert ? 'rgba(0,0,0,0.4)' : colorGainsboro,
-      borderRadius: '0.25rem',
-      padding: '0.125rem',
-      margin: '-0.125rem',
-      textDecoration: 'underline',
+      backgroundColor: invert ? "rgba(0,0,0,0.4)" : colorGainsboro,
+      borderRadius: "0.25rem",
+      padding: "0.125rem",
+      margin: "-0.125rem",
+      textDecoration: "underline",
     },
-    '&:focus': {
+    "&:focus": {
       border: `0.125rem solid ${invert ? colorWhite : colorGreyRaven}`,
-      padding: '0.125rem',
-      margin: '-0.25rem', // (border + padding) * -1
-      borderRadius: '0.25rem',
-      outline: 'none',
+      padding: "0.125rem",
+      margin: "-0.25rem", // (border + padding) * -1
+      borderRadius: "0.25rem",
+      outline: "none",
     },
-  }
-}
+  };
+};
 const StyledLink = styled.a(
   base,
   {
-    '& svg': {
-      transition: 'transform 150ms ease-in-out',
+    "& svg": {
+      transition: "transform 150ms ease-in-out",
     },
-    '&:hover svg': {
-      transform: 'scale(1.1, 1.1)',
+    "&:hover svg": {
+      transform: "scale(1.1, 1.1)",
     },
-    '&:active svg': {
-      transform: 'scale(1, 1)',
+    "&:active svg": {
+      transform: "scale(1, 1)",
     },
   },
   ({ invert, context }) => {
     if (context.inheritColor) {
       return {
-        '&:link,&:visited': {
-          color: 'inherit',
+        "&:link,&:visited": {
+          color: "inherit",
         },
-      }
+      };
     }
     if (invert) {
       return {
-        '&:link,&:visited': {
+        "&:link,&:visited": {
           color: colorWhite,
         },
-      }
+      };
     }
-    return {}
+    return {};
   },
   states,
   ({ hasIcon }) => {
     if (hasIcon) {
       return {
-        display: 'inline-block',
-        '& > svg': {
-          verticalAlign: 'bottom',
+        display: "inline-block",
+        "& > svg": {
+          verticalAlign: "bottom",
         },
-      }
+      };
     }
-    return {}
+    return {};
   }
-)
-
+);
 
 const Link = (
-  { reactRouterLinkComponent, invert, children, forwardedRef, icon: Icon, iconPosition, ...rest },
+  {
+    reactRouterLinkComponent,
+    invert,
+    children,
+    forwardedRef,
+    icon: Icon,
+    iconPosition,
+    ...rest
+  },
   context
 ) => {
-  if (!(reactRouterLinkComponent && rest.to) && (reactRouterLinkComponent || rest.to)) {
-    warn('Link', 'The props `reactRouterLinkComponent` and `to` must be used together.')
+  if (
+    !(reactRouterLinkComponent && rest.to) &&
+    (reactRouterLinkComponent || rest.to)
+  ) {
+    warn(
+      "Link",
+      "The props `reactRouterLinkComponent` and `to` must be used together."
+    );
   }
 
   const renderChildren = useCallback(() => {
     if (Icon) {
       return (
         <>
-          {iconPosition === 'left' && (
-            <Icon color={invert ? 'white' : 'greyShark'} style={{ marginRight: '0.5rem' }} />
+          {iconPosition === "left" && (
+            <Icon
+              color={invert ? "white" : "greyShark"}
+              style={{ marginRight: "0.5rem" }}
+            />
           )}
           {children}
-          {iconPosition === 'right' && (
-            <Icon color={invert ? 'white' : 'greyShark'} style={{ marginLeft: '0.25rem' }} />
+          {iconPosition === "right" && (
+            <Icon
+              color={invert ? "white" : "greyShark"}
+              style={{ marginLeft: "0.25rem" }}
+            />
           )}
         </>
-      )
+      );
     }
-    return children
-  }, [children, Icon, iconPosition, invert])
+    return children;
+  }, [children, Icon, iconPosition, invert]);
 
   return (
     <StyledLink
       {...safeRest(rest)}
-      as={reactRouterLinkComponent || 'a'}
+      as={reactRouterLinkComponent || "a"}
       invert={invert}
       context={context}
       ref={forwardedRef}
@@ -120,18 +144,21 @@ const Link = (
     >
       {renderChildren()}
     </StyledLink>
-  )
-}
+  );
+};
 Link.propTypes = {
-  reactRouterLinkComponent: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
+  reactRouterLinkComponent: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.object,
+  ]),
   to: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
   href: PropTypes.string,
   invert: PropTypes.bool,
   children: PropTypes.node.isRequired,
   forwardedRef: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
-  icon: componentWithName('Dependent', true),
-  iconPosition: PropTypes.oneOf(['left', 'right']),
-}
+  icon: componentWithName("Dependent", true),
+  iconPosition: PropTypes.oneOf(["left", "right"]),
+};
 Link.defaultProps = {
   reactRouterLinkComponent: null,
   to: null,
@@ -139,11 +166,11 @@ Link.defaultProps = {
   invert: undefined,
   forwardedRef: undefined,
   icon: undefined,
-  iconPosition: 'left',
-}
+  iconPosition: "left",
+};
 
 Link.contextTypes = {
   inheritColor: PropTypes.bool,
-}
+};
 
-export default withForwardedRef(Link)
+export default withForwardedRef(Link);
